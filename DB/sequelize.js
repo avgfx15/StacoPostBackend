d03be53afad431dfd7bbbbf0538e655363acebbc
AVGfx <avgfx15@gmail.com>
2026-01-08 15:53:25 +0530

    delete Dockerfile

--- DB/sequelize.js
+++ DB/sequelize.js
@@ -14,6 +14,7 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  dialect: 'mysql',
   logging: false, // Set to console.log to see SQL queries
 });
 

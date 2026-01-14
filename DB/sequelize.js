a8d67764ca55884e3806ed7c101a807795a8b813
AVGfx <avgfx15@gmail.com>
2026-01-14 13:10:37 +0530

    Final Update Done

--- DB/sequelize.js
+++ DB/sequelize.js
@@ -16,9 +16,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: '127.0.0.1', // ✅ FORCE TCP
-    port: 3306,
-    dialect: 'mysql',
+    host: process.env.DB_HOST, // ✅ FORCE TCP
+    port: process.env.DB_PORT ? Number.parseInt(process.env.DB_PORT, 10) : 3306,
+    dialect: process.env.DB_DIALECT,
     logging: false,
     dialectOptions: {
       connectTimeout: 10000,
